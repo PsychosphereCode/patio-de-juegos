b3fe2ab54761dee8fb9cbe2e431d75b974220516
PsychosphereCode <fabian.rottens@gmail.com>
2022-03-04 11:15:49 -0300

    Primer update

---
+++ server.py
@@ -0,0 +1,32 @@
+from turtle import color
+from flask import Flask, render_template
+
+
+
+app= Flask(__name__)
+
+@app.route('/play')
+
+def nivel_1():
+
+    return render_template ('index.html', num = 3, color = "blue")
+
+
+@app.route ('/play/<int:num>')
+
+def nivel_2(num):
+    return render_template("index.html", num = num, color="blue")
+
+    
+@app.route ('/play/<int:num>/<string:color>')
+
+def nivel_3 (num, color):
+
+    return render_template ("index.html", num = num, color = color)
+
+
+if __name__ == "__main__":
+
+    app.run(debug=True)
+
+
